db8cfbc0468536d42c1caf517ed875bf68aa3e4f
hikmet <aydin96tr@gmail.com>
2023-01-02 21:31:55 +0300

    added event test

--- src/tests/TodoList.test.tsx
+++ src/tests/TodoList.test.tsx
@@ -1,4 +1,5 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, waitFor } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 import { TodoList } from "../components/TodoList";
 
 describe("TodoList test", () => {
@@ -29,5 +30,33 @@ describe("TodoList test", () => {
     const todoItems = screen.getAllByTitle(/todo/i);
     todoItems.forEach((item, i) => expect(item).toHaveTextContent(mockTodos[i]["name"]));
     expect(todoItems).toHaveLength(mockTodos.length);
+  });
+
+  it("should delete todo when clicked", async () => {
+    const user = userEvent.setup();
+
+    const mockTodos = [
+      { name: "test", id: "0" },
+      { name: "jesst", id: "1" },
+      { name: "mest", id: "2" },
+    ];
+    const mocksetTodos = jest.fn();
+    const { queryByText, rerender, debug } = render(<TodoList todos={mockTodos} setTodos={mocksetTodos} />, {});
+
+    const todoItem = queryByText(/mest/i);
+    todoItem && (await user.click(todoItem));
+    rerender(
+      <TodoList
+        todos={[
+          { name: "test", id: "0" },
+          { name: "jesst", id: "1" },
+        ]}
+        setTodos={mocksetTodos}
+      />
+    );
+    debug();
+    await waitFor(() => expect(todoItem).not.toBeInTheDocument());
+
+    expect(mocksetTodos).toHaveBeenCalledTimes(1);
   });
 });
